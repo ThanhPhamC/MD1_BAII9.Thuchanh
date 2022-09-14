f0093681bcd7bd490d3b0f5dce1e0f167727a79d
Phạm Thanh <thanhphamhumg@gmail.com>
2022-09-14 15:10:33 +0700

    thuchanh

---
+++ MyDate.js
@@ -0,0 +1,37 @@
+class MyDate {
+    day;
+    mon;
+    year;
+
+    constructor(day, mon, year) {
+        this.day = day;
+        this.mon = mon;
+        this.year = year;
+    }
+    setDay(day){
+        this.day=day
+    }
+    getDay(){
+        return this.day
+    }
+    setMon(mon){
+        this.mon=mon
+    }
+    getMon(){
+        return this.mon
+    }
+    setYear(year){
+        this.year=year
+    }
+    getYear(){
+        return this.year
+    }
+    setDate(day,mon,year){
+        this.day=day;
+        this.mon=mon;
+        this.year=year;
+    }
+    toString(){
+        return this.day+"/"+this.mon+"/"+this.year
+    }
+}
